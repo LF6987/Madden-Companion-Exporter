46c1600465e579bbdcbd0a2279448b927bb2f480
LF6987 <39294180+LF6987@users.noreply.github.com>
2018-09-08 00:38:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-// const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+// const serviceAccount = require("path/to/serviceAccountKey.json");
 
 // TODO: Enter your database url from firebase
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com/"
+//   databaseURL: "https://ebowl-37c5a.firebaseio.com"
 // });
 
 
@@ -131,3 +131,23 @@ app.post('/:username/:platform/:leagueId/team/:teamId/roster', (req, res) => {
 });
 
 app.listen(app.get('port'), function () { return console.log('Madden Data is running on port', app.get('port')) });
+
+
+
+
+
+
+
+<script src="https://www.gstatic.com/firebasejs/5.4.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDwtW0KwwVUZNEODBCvrHveMTm1URdH5B8",
+    authDomain: "ebowl-37c5a.firebaseapp.com",
+    databaseURL: "https://ebowl-37c5a.firebaseio.com",
+    projectId: "ebowl-37c5a",
+    storageBucket: "ebowl-37c5a.appspot.com",
+    messagingSenderId: "49186669936"
+  };
+  firebase.initializeApp(config);
+</script>
